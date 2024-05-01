46e35c557767d636313c0fa23a296caa41052564
mastotte <mastotte@ucsc.edu>
2024-05-01 12:15:29 -0700

    max's functions

--- cpu.h
+++ cpu.h
@@ -11,4 +11,16 @@ class CPU {
                 programCounter += 4+4*immediate;
             }
         }
+        void add(int reg_a, int reg_b, int reg_c){
+            registers[reg_c] = registers[reg_a] + registers[reg_b];
+        }
+        void shiftRightArithmetic(int reg_b, int reg_c, int shift_value){
+            registers[reg_c] = (signed) registers[reg_b] >> shift_value;
+        }
+        void and(int reg_a, int reg_b, int reg_c){
+            registers[reg_c] = registers[reg_a] & registers[reg_b];
+        }
+        void jumpRegister(int reg_a){
+            programCounter = registers[reg_a];
+        }
 }
