86e02bea635e9acc0f6fddc41e601e3b5b0f511e
classicallydaniel <131523241+classicallydaniel@users.noreply.github.com>
2024-05-02 23:46:18 -0700

    Add name comments to cpp.h (#1)
    Co-authored-by: Daniel Kim <dtk.danielkim@gmail.com>

--- cpu.h
+++ cpu.h
@@ -6,11 +6,19 @@ class CPU {
         uint16_t programCounter;
         uint16_t *registers;
     public:
+        //--------------------- 2.1.1-2.14 (Milan) ---------------------
         void branchOnEqual(int reg_a, int reg_b, int immediate){
             if (registers[reg_a] == registers[reg_b]){
                 programCounter += 4+4*immediate;
             }
         }
+
+        //--------------------- 2.1.5-2.1.8 (Daniel) ---------------------
+        
+
+        //--------------------- 2.1.9-2.2.3 (Josh) ---------------------
+
+        //--------------------- 2.2.4-2.2.7 (Max) ---------------------
         void add(int reg_a, int reg_b, int reg_c){
             registers[reg_c] = registers[reg_a] + registers[reg_b];
         }
@@ -23,7 +31,7 @@ class CPU {
         void jumpRegister(int reg_a){
             programCounter = registers[reg_a];
         }
-
+        //--------------------- 2.2.8-2.2.10 (Omid) ---------------------
         void shiftLeftLogical (int reg_b, int reg_c, int shift_value){
             registers[reg_c] = registers[reg_b] << shift_value;
         }
