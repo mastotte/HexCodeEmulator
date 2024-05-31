6f9ea2acfaa4892c4b6807ab376fd42724ca1fcd
root <root@DESKTOP-1K1IN9J>
2024-05-30 20:28:37 -0700

    cpu print

--- cpu.cpp
+++ cpu.cpp
@@ -17,17 +17,26 @@
 // Instruction Functions
 //--------------------- 2.1.1-2.14 (Milan) ---------------------
 void CPU::branchOnEqual(int reg_a, int reg_b, int immediate, MEMORY memory) {
+    printf("entering branchonequal\n");
     if (registers[reg_a] == registers[reg_b]) {
         programCounter -= 4;
         programCounter += 4 + (4 * immediate);
     }
+        printf("exiting branchonequal\n");
+
 }
 
 void CPU::loadWord(int reg_a, int reg_b, int immediate, MEMORY memory) {
+                printf("entering loadword\n");
+
     registers[reg_b] = memory.read16(registers[reg_a] + immediate);
+            printf("exiting loadword\n");
+
 }
 
 void CPU::loadByteUnsigned(int reg_a, int reg_b, int immediate, MEMORY memory) {
+        printf("entering loadByteUnsigned\n");
+
     registers[reg_b] = memory.read8(registers[reg_a] + immediate);
     // // Read a byte from memory
     // uint16_t byteFromMemory = memory[registers[reg_a] + immediate];
@@ -37,103 +46,152 @@ void CPU::loadByteUnsigned(int reg_a, int reg_b, int immediate, MEMORY memory) {
 
     // // Store the extracted value into registers[reg_b]
     // registers[reg_b] = lower8Bits;
+
+                printf("exiting loadByteUnsigned\n");
+
 }
 
 void CPU::jump(int reg_a, int reg_b, int immediate, MEMORY memory) {
+                    printf("entering jump\n");
     programCounter -= 4;
     programCounter = 4 * immediate;
+
+                printf("exiting jump\n");
+
     // std::cout << "j" << std::endl;
 }
 //--------------------- 2.1.5-2.1.8 (Daniel) ---------------------
 void CPU::storeWord(int reg_a, int reg_b, int immediate, MEMORY memory) {
+                        printf("entering writ16\n");
+
     memory.write16(registers[reg_a] + immediate, registers[reg_b]);
     // std::cout << "storew" << std::endl;
-    
+                            printf("exiting writ16\n");
+
     //memory[registers[reg_a] + immediate] = registers[reg_b];
 }
 
 void CPU::storeByte(int reg_a, int reg_b, int immediate, MEMORY memory) {
     // // Extract the lower 8 bits from registers[reg_b]
     // uint8_t lower8Bits = registers[reg_b] & 0xFF;
+                        printf("entering storebyte\n");
 
     // // Store the lower 8 bits into memory at the specified address
     // memory[registers[reg_a] + immediate] = lower8Bits;
     memory.write8(registers[reg_a] + immediate, registers[reg_b]);
+                            printf("exiting storebyte\n");
+
     // std::cout << "store" << std::endl;
 }
 
 void CPU::orImmediate(int reg_a, int reg_b, int immediate, MEMORY memory) {
+                            printf("entering ori\n");
+
     registers[reg_b] = (registers[reg_a] | immediate);
+                            printf("exiting ori\n");
+
     // std::cout << "ori" << std::endl;
 }
 
 void CPU::branchOnNotEqual(int reg_a, int reg_b, int immediate, MEMORY memory) {
+                                printf("entering bne\n");
+
     if (registers[reg_a] != registers[reg_b]) {
         programCounter -= 4;
         programCounter += 4 + 4 * immediate;
     }
+                                    printf("exiting bne\n");
+
     // std::cout << "benq" << std::endl;
 }
 //--------------------- 2.1.9-2.2.3 (Josh) ---------------------
 void CPU::jumpAndLink(int reg_a, int reg_b, int immediate, MEMORY memory) {
+                                    printf("entering jal\n");
+
     registers[31] = programCounter + 4;
     programCounter -= 4;
     programCounter = 4 * immediate;
-    
+                                        printf("exiting jal\n");
+
     // std::cout << "jal" << std::endl;
 }
 
 void CPU::subtract(int reg_a, int reg_b, int reg_c, int shift_value, MEMORY memory) {
+                                        printf("entering sub\n");
+
     registers[reg_c] = registers[reg_a] - registers[reg_b];
+                                            printf("exiting sub\n");
+
     // std::cout << "sub" << std::endl;
 }
 
 void CPU::or_Op(int reg_a, int reg_b, int reg_c, int shift_value, MEMORY memory) {
+                                            printf("entering orop\n");
+
     registers[reg_c] = registers[reg_a] | registers[reg_b];
     // std::cout << "or" << std::endl;
+                                                printf("exiting orop\n");
+
 }
 
 void CPU::nor(int reg_a, int reg_b, int reg_c, int shift_value, MEMORY memory) {
+                                                printf("entering nor\n");
+
     registers[reg_c] = !(registers[reg_a] | registers[reg_b]);
+                                                    printf("exiting nor\n");
+
     // std::cout << "nor" << std::endl;
 }
 //--------------------- 2.2.4-2.2.7 (Max) --------------------
 void CPU::add(int reg_a, int reg_b, int reg_c, int shift_value, MEMORY memory) {
+     std::cout << "entering add" << std::endl;
     // std::cout << "add" << std::endl;
     registers[reg_c] = registers[reg_a] + registers[reg_b];
+     std::cout << "exiting add" << std::endl;
 }
 
 void CPU::shiftRightArithmetic(int reg_a,int reg_b, int reg_c, int shift_value, MEMORY memory) {
+     std::cout << "entering shiftRightArithmetic" << std::endl;
     // std::cout << "sra" << std::endl;
     registers[reg_c] = (signed)registers[reg_b] >> shift_value;
+     std::cout << "exiting shiftRightArithmetic" << std::endl;
 }
 
 void CPU::bitwise_and(int reg_a, int reg_b, int reg_c, int shift_value, MEMORY memory) {
+     std::cout << "entering bitwise_and" << std::endl;
     registers[reg_c] = registers[reg_a] & registers[reg_b];
     // std::cout << "and" << std::endl;
+     std::cout << "exiting bitwise_and" << std::endl;
 }
 
 void CPU::jumpRegister(int reg_a, int reg_b, int reg_c, int shift_value, MEMORY memory) {
+     std::cout << "entering jumpRegister" << std::endl;
     programCounter -= 4;
     programCounter = registers[reg_a];
     // std::cout << "jr" << std::endl;
+     std::cout << "exiting jumpRegister" << std::endl;
 }
 //--------------------- 2.2.8-2.2.10 (Omid) ---------------------
 void CPU::shiftLeftLogical(int reg_a,int reg_b, int reg_c, int shift_value, MEMORY memory) {
+     std::cout << "entering shiftLeftLogical" << std::endl;
     registers[reg_c] = (unsigned)registers[reg_b] << shift_value;
     // std::cout << "sll" << std::endl;
+     std::cout << "exiting shiftLeftLogical" << std::endl;
 }
 
 void CPU::shiftRightLogical(int reg_a,int reg_b, int reg_c, int shift_value, MEMORY memory) {
+     std::cout << "entering shiftRightLogical" << std::endl;
     registers[reg_c] = (unsigned)registers[reg_b] >> shift_value;
     // std::cout << "srl" << std::endl;
+     std::cout << "exiting shiftRightLogical" << std::endl;
 }
 
 void CPU::setLessThan(int reg_a, int reg_b, int reg_c, int shift_value, MEMORY memory) {
+    std::cout << "entering setLessThan" << std::endl;
     registers[reg_c] = (registers[reg_a] < registers[reg_b]);
     // std::cout << "slt" << std::endl;
+     std::cout << "exiting setLessThan" << std::endl;
 }
-
 
 // read and write functions
 
